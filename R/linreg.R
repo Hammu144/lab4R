36cea768b0bfd115ec6f55690896c9e68196867c
Hammu144 <hamzamunir00@gmail.com>
2022-10-01 23:56:23 +0200

    code updation

--- R/linreg.R
+++ R/linreg.R
@@ -5,7 +5,7 @@
 #' @field hat_B matrix.
 #' @field hat_y matrix.
 #' @field hat_e matrix.
-#' @field df integer.
+#' @field df numeric
 #' @field hat_var_2 matrix.
 #' @field hat_var_hat_B matrix.
 #' @field data data.frame.
@@ -16,7 +16,7 @@
 #' @export linreg
 #' @exportClass linreg
 #' @importFrom methods new setRefClass
-#'
+#' @import ggplot2
 #' @examples
 #' data(iris)
 #' mod_object <- lm(Petal.Length~Species, data = iris)
@@ -26,7 +26,7 @@
 linreg <- setRefClass("linreg",
                       fields = list(x = "matrix", y = "numeric",
                                     hat_B = "matrix", hat_y = "matrix",
-                                    hat_e = "matrix", df = "integer",
+                                    hat_e = "matrix", df = "numeric",
                                     hat_var_2 = "matrix", hat_var_hat_B = "matrix",
                                     data = "data.frame", formula = "formula",
                                     t_B = "numeric"
@@ -39,14 +39,14 @@ linreg <- setRefClass("linreg",
                           .self$y <-  .self$data[[.self$formula[[2]]]]
                           n <- nrow(.self$x)
                           p <- ncol(.self$x)
-                          .self$df <- n-p
-                          .self$hat_B <- solve((t( .self$x)%*% .self$x))%*%t( .self$x)%*% .self$y
+                          .self$df <- as.numeric(n-p)
+
+                          .self$hat_B <- solve((t( .self$x) %*% .self$x))%*% (t(.self$x ) %*% .self$y)
                           .self$hat_y <- ( .self$x) %*% ( .self$hat_B)
                           .self$hat_e <-  .self$y -  .self$hat_y
                           .self$hat_var_2 <- (t(.self$hat_e)%*%.self$hat_e) / .self$df
 
-                          .self$hat_var_hat_B <- ((.self$hat_var_2)^(2))[1,1] * (solve(t(.self$x)%*%.self$x))
-
+                          .self$hat_var_hat_B <- as.vector(.self$hat_var_2) * solve(t(.self$x) %*% .self$x)
                           for (i in 1:length(.self$hat_B))
                           {
                             tem_vec <- .self$hat_B[i] /(sqrt(.self$hat_var_hat_B[i,i]))
@@ -84,36 +84,48 @@ linreg <- setRefClass("linreg",
                         },
 
                         summary = function(){
-                          v1 <- .self$hat_B
-                          v2 <-sqrt(abs(diag(.self$hat_var_hat_B)))
-                          v3 <- .self$t_B
+                          vect1 <- .self$hat_B
+                          vect2 <-sqrt(abs(as.vector(diag(.self$hat_var_hat_B))))
+                          vect3 <- .self$t_B
                           v4 <- c()
                           for(i in 1:length(.self$t_B)){
                             p = pt(.self$t_B[i], .self$df, lower.tail = FALSE, log.p = FALSE)
                             if((p < 0.01) & (p < 0.05)){v4 <- append(v4,"***")}
                             else if((p > 0.01) & (p < 0.05)){v4 <-append(v4,"**")}
                             else if((p > 0.05) & (p < 0.05)){v4 <-append(v4,"*")}
-                            else {v4 <-append(v4," ")}
+                            else {v4 <-append(v4,"")}
 
                           }
-                           m <- matrix(c(v1,v2,v3,v4 ),nrow = 3, ncol = 4, byrow = FALSE)
-                           rownames(m) <-
-                             c("Intercept", "Speciesversicolor", "Speciesvirginica")
-                           colnames(m) <-
+                           mat <- matrix(c(vect1,vect2,vect3,v4 ),nrow = 3, ncol = 4, byrow = FALSE)
+
+
+                           rownames(mat) <- row.names(.self$hat_B)
+                           nameVec <- rownames(mat)
+                             # c("Intercept", "Speciesversicolor", "Speciesvirginica")
+                           colnames(mat) <-
                              c("Estimate", "Std.Error", "t value", "pr(>|t|)")
-                           return(m)
+                           # return(mat)
+
+                           for(item in 1:nrow(mat)){
+                            cat(nameVec[item], "")
+                             cat(mat[item,], "\n")
+
+                           }
+
+                           cat(paste0("Residual standard error: ", sqrt(.self$hat_var_2), " on ", .self$df , " degrees of freedom"))
 
 
 
-                          cat(paste0("Residual standard error: ", sqrt(.self$hat_var_2), " on ", .self$df , " degrees of freedom"))
+
 
                         },
                         print = function(){
                         n_formula <- as.character(.self$formula)
-                        cat(paste0("lm(formula =" ,n_formula[2], " " , n_formula[1], " " ,n_formula[3], " data = iris)"))
+                        cat("linreg(formula = ", format(.self$formula), ", data = iris)", "\n", sep = "")
 
-                        cat(paste0(rownames(.self$hat_B), "/n"))
-                        cat(paste0(hat_B))
+                        cat(paste0(rownames(.self$hat_B)))
+                        cat("\n")
+                        cat(paste0(.self$hat_B))
 
 
 
@@ -124,12 +136,4 @@ linreg <- setRefClass("linreg",
                       )
 )
 
-
-
-# data(iris)
-# mod_object <- lm(Petal.Length~Species, data = iris)
-# print(mod_object)
-# summary(mod_object)
-#
-#
 
